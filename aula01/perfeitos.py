31d76c4de45492e5aa5f211837bcd623a9dc7d83
afonsocastro <afonsocastro@ua.pt>
2020-10-13 20:08:25 +0100

    first class completed

---
+++ aula01/perfeitos.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+maximum_number = 100
+
+def isPerfect(value):
+    lista_divisores = []
+    for i in range(1, value):
+        if value % i == 0:
+            lista_divisores.append(i)
+        else:
+            continue
+
+    if sum(lista_divisores) == value:
+        return True
+    else:
+        return False
+
+def main():
+    print("Starting to compute perfect numbers up to " + str(maximum_number))
+
+    for i in range(0, maximum_number):
+        if isPerfect(i):
+            print('Number ' + str(i) + ' is perfect.')
+
+
+if __name__ == "__main__":
+    main()
--- aula01/primo.py
+++ aula01/primo.py
@@ -1,16 +1,29 @@
+#!/usr/bin/env python
+
+import colorama
+
 maximum_number = 50
 def isPrime(value):
-    # <Fill the blank>
-    return
+    for i in range(2, value):
+        if value % i == 0:
+            return False
+        else:
+            continue
+    return True
 
 def main():
     print("Starting to compute prime numbers up to " + str(maximum_number))
 
-    for i in range(0, maximum_number):
+    for i in range(1, maximum_number):
         if isPrime(i):
-            print('Number ' + str(i) + ' is prime.')
+            print(colorama.Fore.GREEN + 'Number ' + str(i) + ' is prime.')
         else:
-            print('Number ' + str(i) + ' is not prime.')
+            print('Number ' + str(i) + ' is not prime. Here are all the dividers:')
+            for ii in range(1, i+1):
+                if i % ii == 0:
+                    print("\n" + str(ii))
+
+
 
 if __name__ == "__main__":
     main()

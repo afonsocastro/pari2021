da9bf3b4f0a1748867c64f55c5f31ed1f21023e1
zebastos <zbastos@gmail.com>
2020-10-21 10:23:35 +0100

    part 03 ex1 -> Colorama

--- parte03/main.py
+++ parte03/main.py
@@ -2,10 +2,13 @@
 
 from time import time, ctime
 from math import sqrt
+from colorama import Fore
+from colorama import Back
+
 
 #confirmar se esccreve no repositorio bfd
 def main():
-    print("This is Ex1 and the current date is " + ctime())
+    print("This is" +  Fore.RED + " Ex1" + Fore.RESET + " the current date is " + Back.LIGHTYELLOW_EX + ctime() + Back.RESET)
 
     tic = time()
     for i in range(0, 50000001):

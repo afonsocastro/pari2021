c1a75283089d7798a74f8ecee82af68f115179cb
afonsocastro <afonsocastro@ua.pt>
2020-10-22 11:30:34 +0100

    new laptop working test

--- parte03/classes.py
+++ parte03/classes.py
@@ -14,7 +14,6 @@ class Complex:
     def multiply(self, y):
         (self.r, self.i) = (self.r*y.r - self.i*y.i, self.i*y.r + self.r*y.i)
         return self
-        # return Complex(self.r*y.r - self.i*y.i, self.i*y.r + self.r*y.i)
 
     def __str__(self):# esta funcao serve para fazer o print
         return str(self.r) + "+" + str(self.i) + "i" if self.i >= 0 else str(self.r) + str(self.i) + "i"
@@ -24,9 +23,6 @@ def main():
     # declare two instances of class two complex numbers as tuples of size two
     c1 = Complex(5, 3)  # use order when not naming
     c2 = Complex(i=7, r=-2)  # if items are names order is not relevant
-
-    # c3 = c1.multiply(c2)
-    # print(c3)  # uses the __str__ method in the class
 
     # Test add
     print(c1)  # uses the __str__ method in the class
--- parte03/main.py
+++ parte03/main.py
@@ -6,8 +6,8 @@ from colorama import Fore
 from colorama import Back
 from colorama import Style
 
-
 #confirmar se esccreve no repositorio bfd
+
 def main():
     print("This is" +  Fore.RED + " Ex1" + Fore.RESET + " the current date is " + Back.LIGHTYELLOW_EX + ctime() + Style.RESET_ALL )
 

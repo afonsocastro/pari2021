bcf1b93129df2cdf94de2367ba1a02b684749282
zebastos <zbastos@gmail.com>
2020-10-28 11:10:11 +0000

    ex1.py

--- parte05/ex1.py
+++ parte05/ex1.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python
 
+
+
 import cv2
+import argparse
 
 def main():
 
-    image_filename = '/home/ze/Desktop/aulas_20-21/aulas do prof/pari_20-21/Parte05/images/atlascar2.png'
-    image = cv2.imread(image_filename, cv2.IMREAD_COLOR) # Load an image
+    parser = argparse.ArgumentParser(description='Ler imagens')
+    parser.add_argument('-img1', help='path para img1')
+    parser.add_argument('-img2', help='path para img2')
+    args = parser.parse_args()
+    args=vars(args)
+    print(args)
 
-    cv2.imshow('window', image)  # Display the image
-    cv2.waitKey(0) # wait for a key press before proceeding
+    #image_filename = '/images/atlascar2.png'
 
+    img1=cv2.imread(args['img1'],1)
+    img2=cv2.imread(args['img2'],1)
+
+    window_name='window'
+    flip_flop=False
+
+    while True:
+        if flip_flop:
+            cv2.imshow(window_name, img1)  # Display the image
+            flip_flop=False
+        else:
+            v2.imshow(window_name, img2)  # Display the image
+            flip_flop=True
+
+        cv2.waitKey(3000) # wait for a key press before proceeding
 
 if __name__ == '__main__':
     main()
